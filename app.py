d4ac1c77fa2e3a35675c24c08ea9148576c77d5c
MukthaSS <137072191+MukthaSS@users.noreply.github.com>
2025-07-13 19:36:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 # Load the trained model
 model = load_model("churn_model.h5")
 
